660fe1737b4a1d46478c0b6f48181e9c2a2fabe3
Zafar6699 <webzafar6699@gmail.com>
2021-10-09 20:43:28 +0500

    asd

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,8 @@ export default {
     },
 
     // Global CSS: https://go.nuxtjs.dev/config-css
-    css: ["@/assets/styles/style.scss"],
+
+    css: ["@/assets/styles/style.scss", "@/assets/styles/font.css"],
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
     plugins: [],
