cb7851c51482212c7a2a25bae4a7d419184d3e08
Zafar6699 <webzafar6699@gmail.com>
2021-10-09 21:23:23 +0500

    asd

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
     css: ["@/assets/styles/style.scss", "@/assets/styles/font.css"],
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-    plugins: [],
+    plugins: ["@/plugins/clickOut.js"],
 
     // Auto import components: https://go.nuxtjs.dev/config-components
     components: true,
