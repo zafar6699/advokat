3eb93e30062667045d9b2fd7cbd47a3278f360b3
Zafar6699 <webzafar6699@gmail.com>
2021-10-10 17:16:14 +0500

    asd

--- pages/managment/index.vue
+++ pages/managment/index.vue
@@ -2,6 +2,75 @@
     <div class="managment">
         <div class="content-title">
             <h3>Фойдаланувчиларни бошқариш</h3>
+
+            <nuxt-link to="/" class="primary btn-orange">Яратиш</nuxt-link>
+        </div>
+
+        <div class="card-row">
+            <div class="card-3">
+                <div class="form-group">
+                    <h4>Роли</h4>
+                    <div class="input-div">
+                        <select>
+                            <option value="1">Суппер админ</option>
+                            <option value="2">Toshkent</option>
+                            <option value="3">Xorazm</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="card-3">
+                <div class="form-group">
+                    <h4>Логин</h4>
+                    <div class="input-div">
+                        <input type="text" />
+                    </div>
+                </div>
+            </div>
+            <div class="card-3">
+                <div class="form-group">
+                    <h4>Ф.И.Ш</h4>
+                    <div class="input-div">
+                        <input type="text" />
+                    </div>
+                </div>
+            </div>
+            <div class="card-3">
+                <div class="form-group">
+                    <h4>Вилоят</h4>
+                    <div class="input-div">
+                        <select>
+                            <option value="1">Самарқанд вилояти</option>
+                            <option value="2">Toshkent</option>
+                            <option value="3">Xorazm</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="card-3">
+                <div class="form-group">
+                    <h4>Туман</h4>
+                    <div class="input-div">
+                        <select>
+                            <option value="1">Норин тумани</option>
+                            <option value="2">Toshkent</option>
+                            <option value="3">Xorazm</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="card-3">
+                <div class="form-group">
+                    <h4>Статус</h4>
+                    <div class="input-div">
+                        <select>
+                            <option value="1">Тўхтатилган</option>
+                            <option value="2">Toshkent</option>
+                            <option value="3">Xorazm</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -13,19 +82,11 @@ export default {
         return {
             route: [
                 {
-                    url: "history",
-                    value: {
-                        uz: "Route_uz",
-                        kr: "Route_kr",
-                        ru: "Route_ru"
-                    }
-                },
-                {
                     url: "",
                     value: {
-                        uz: "Batafsil",
-                        kr: "Batafsil",
-                        ru: "Batafsil"
+                        uz: "Foydalanuvchilarni boshqarish",
+                        kr: "Фойдаланувчиларни бошқариш",
+                        ru: "Управление пользователями"
                     }
                 }
             ]
